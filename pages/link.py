10c9445c55f29b3d82d1251bbddd977fad59def1
CyberSh0gun <153566063+CyberSh0gun@users.noreply.github.com>
2025-05-21 21:07:15 -0300

    Add files via upload

---
+++ pages/link.py
@@ -0,0 +1,12 @@
+import os
+import streamlit as st
+import streamlit.components.v1 as components
+
+_allow_html=True
+#)
+st.title("Cardápio")
+
+st.markdown(
+    '<a href="http://localhost:5001/html/index.html" target="_blank">Abrir HTML</a>',
+    unsafe_allow_html=True
+)
