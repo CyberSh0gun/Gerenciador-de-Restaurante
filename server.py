1b2dcbb0d23298afb3b5eadb694dab7954444a26
CyberSh0gun <153566063+CyberSh0gun@users.noreply.github.com>
2025-05-21 22:24:40 -0300

    Add files via upload

--- server.py
+++ server.py
@@ -7,6 +7,9 @@ app = Flask(__name__, static_folder='static')
 @app.route('/html/<path:filename>')
 def serve_html(filename):
     return send_from_directory(app.static_folder, filename)
+st.markdown(
+    '<a href="https://html-flask-server.onrender.com" target="_blank">Abrir HTML</a>',
+    unsafe_allow_html=True)
 
 #if __name__ == '__main__':
  #   app.run(port=5001)
